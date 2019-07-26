cac3d19e88b5d53b04941ebde61b0ec46d058c6f
jurajmajerik <juro.majerik@gmail.com>
2019-07-26 16:05:47 +0200

    Generate dummy data programmatically

--- src/_data/historicalData.js
+++ src/_data/historicalData.js
@@ -1,12 +1,6 @@
 /* eslint-disable */
-export const countHistoricalAll = [
-  50, 70, 60, 90, 100, 80, 70, 80, 60, 70, 70, 60, 80, 90, 70, 80, 70, 80, 60, 70, 70, 70, 60, 90, 70, 80, 70, 60, 80, 60,
-];
+export const countHistoricalAll = Array.from({ length: 30 }, () => Math.floor(Math.random() * 50) + 50);
 
-export const countHistoricalCountry = [
-  5, 7, 6, 9, 1, 8, 2, 8, 6, 7, 7, 6, 2, 9, 7, 8, 7, 8, 3, 7, 7, 7, 6, 1, 7, 8, 4, 6, 8, 6,
-];
+export const countHistoricalCountry = Array.from({ length: 30 }, () => Math.floor(Math.random() * 5) + 5);
 
-export const datesHistorical = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-];
+export const datesHistorical = ([...Array(30).keys()].map(x => x++));
